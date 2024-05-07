96bdd6e1ae8b1c6514b358af2e5d157dc2059834
novru <novru@EMINSIDEAPAD>
2024-05-07 20:30:36 +0400

    Some Configurations with DataBase and other

---
+++ Infrastructure/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>().ToContainer("Courses");
+        modelBuilder.Entity<User>().ToContainer("Users");
+        modelBuilder.Entity<Comment>().ToContainer("Comments");
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
