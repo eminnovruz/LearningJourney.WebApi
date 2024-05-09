e9421e2b0c26eaabad8bd11025b7f08e031a9d33
novru <novru@EMINSIDEAPAD>
2024-05-09 14:58:02 +0400

    IReadRepository, finished and configured with ReadRepository Class

---
+++ Core/Application/Repositories/Repository/IReadRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models.Common;
+using System.Linq.Expressions;
+
+namespace Application.Repositories.Repository;
+
+public interface IReadRepository<T> where T : BaseEntity
+{
+    IEnumerable<T> GetAll(bool tracking = true);
+    IEnumerable<T> GetWhere(Expression<Func<T, bool>> expression);
+
+    Task<T> GetAsync(string id);
+    Task<T> GetAsync(Expression<Func<T, bool>> expression);
+
+    // Extra methods
+    T Get(string id);
+}
---
+++ Infrastructure/Persistence/Repositories/Repository/ReadRepository.cs
@@ -0,0 +1,70 @@
+﻿using Application.Repositories.Repository;
+using Domain.Models.Common;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Context;
+using System.Linq.Expressions;
+
+namespace Persistence.Repositories.Repository;
+
+public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+{
+    private readonly AppDbContext _context;
+
+    public ReadRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    DbSet<T> Table => _context.Set<T>();
+
+    public T Get(string id)
+    {
+        var entity = Table.FirstOrDefault(x => x.Id == id);
+
+        if (entity is null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        return entity;
+    }
+
+    public IEnumerable<T> GetAll(bool tracking = true)
+    {
+        if(tracking)
+        {
+            return Table.ToList();
+        }
+
+        return Table.AsNoTracking().ToList();
+    }
+
+    public async Task<T> GetAsync(string id)
+    {
+        var entity = await Table.FirstOrDefaultAsync(x => x.Id == id);
+        
+        if(entity is null)
+        {
+            throw new ArgumentNullException(nameof(entity));
+        }
+
+        return entity;
+    }
+
+    public async Task<T> GetAsync(Expression<Func<T, bool>> expression)
+    {
+        var entity = await Table.FirstOrDefaultAsync(expression);
+
+        if (entity is null)
+        {
+            throw new ArgumentNullException(nameof(entity));
+        }
+
+        return entity;
+    }
+
+    public IEnumerable<T> GetWhere(Expression<Func<T, bool>> expression)
+    {
+        return Table.Where(expression);
+    }
+}
