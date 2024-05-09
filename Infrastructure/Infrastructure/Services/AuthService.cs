9ac5345acfc4c7e00fbcfaca7df8ad19e008e6e7
novru <novru@EMINSIDEAPAD>
2024-05-09 16:11:31 +0400

    New PassHashService

--- Infrastructure/Infrastructure/Services/AuthService.cs
+++ Infrastructure/Infrastructure/Services/AuthService.cs
@@ -1,6 +1,29 @@
-﻿namespace Infrastructure.Services;
+﻿using Application.Models.Requests;
+using Application.Models.Responses;
+using Application.Services;
+using Domain.Models;
 
-public class AuthService
+namespace Infrastructure.Services;
+
+public class AuthService : IAuthService
 {
+    public AuthTokenInfo GenerateToken(User user)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<AuthTokenInfo> Login(LoginUserRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AuthTokenInfo> RefreshToken(RefreshTokenRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Register(RegisterUserRequest request)
+    {
+        throw new NotImplementedException();
+    }
 }
